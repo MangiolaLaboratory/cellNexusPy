a66958eca6904e19978ed16aeb3be71f0b9b594c
Michael Milton <michael.r.milton@gmail.com>
2023-02-14 15:50:24 +1100

    Add test

---
+++ test/test_all.py
@@ -0,0 +1,6 @@
+from curated_atlas_query_py import *
+
+def test_get_sce():
+    con, table = get_metadata(cache_dir='/vast/projects/cellxgene_curated/')
+    sce = get_SingleCellExperiment(table.head(1))
+    print(sce)

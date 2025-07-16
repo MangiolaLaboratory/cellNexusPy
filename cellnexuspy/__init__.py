9b932a0b00112bd2294963740a2f53b69d919349
Juan David Henao <judhenaosa@unal.edu.co>
2025-07-16 19:47:39 +0200

    Beta version

--- cellnexuspy/__init__.py
+++ cellnexuspy/__init__.py
@@ -34,8 +34,7 @@ def is_parquet_valid(parquet_file):
 def _get_default_cache_dir() -> Path:
     return Path(user_cache_dir("cellNexusPy"))
 
-
-# helper function to download file over http/https
+    # helper function to download file over http/https
 def _sync_remote_file(full_url: str, output_file: Path):
     if not output_file.exists():
         output_dir = output_file.parent
@@ -66,111 +65,124 @@ def get_metadata(
 def sync_assay_files(
     url: str = ASSAY_URL,
     cache_dir: Path = _get_default_cache_dir(),
-    subdirs: Iterable[str] = [],
-    atlases: Iterable[str] = [],
-    aggregation: Iterable[str] = [],
+    subdir: str = "",
+    atlas: str = "",
+    aggregation: str = "",
     files: Iterable[str] = [],
 ):
-    for atlas in atlases:
-        for subdir in subdirs:
-            for file in files:
-                if aggregation == "single_cell":
-                    sub_url = f"{url}/{atlas}/{subdir}/{file}"
-                else:
-                    sub_url = f"{url}/{atlas}/{aggregation}/{subdir}/{file}"
-                output_filepath = cache_dir / atlas / aggregation / subdir / file
+    for file in files:
+        if aggregation == "single_cell":
+            sub_url = f"{url}/{atlas}/{subdir}/{file}"
+        else:
+            sub_url = f"{url}/{atlas}/{aggregation}/{subdir}/{file}"
+        output_filepath = cache_dir / atlas / aggregation / subdir / file
 
-                if not output_filepath.exists() or os.path.getsize(output_filepath) < MIN_EXPECTED_SIZE:
-                    _sync_remote_file(sub_url, output_filepath)
+        if not output_filepath.exists() or os.path.getsize(output_filepath) < MIN_EXPECTED_SIZE:
+            _sync_remote_file(sub_url, output_filepath)
 
-                yield subdir, output_filepath
-                
-def _filter_obs(data,aggregation,anndata,cells,features):
-    if aggregation == "single_cell":
-        anndata.obs.index = anndata.obs.index.astype(str)
-        cell_ids = cells.astype(str)
-        pattern = '|'.join(re.escape(s) for s in cell_ids)
-        mask = anndata.obs.index.str.contains(pattern, regex=True)
+        yield subdir, output_filepath
+        
+def filter_pseudobulk(file, data):
+    cells = data.filter("file_id_cellNexus_pseudobulk ="  + "'"+str(file).split("/")[-1]+"'").fetchdf()
+    cell_ids = cells["sample_id"].astype(str) + "___" + cells["cell_type_unified_ensemble"].astype(str)
+    anndata = ad.read_h5ad(file)
+    ann = anndata[cell_ids.unique()].copy()
+
+    columns_to_remove = ["cell_id", "cell_type", "file_id_cellNexus_single_cell",
+                     "cell_type_ontology_term_id",
+                     "observation_joinid", "ensemble_joinid",
+                     "nFeature_RNA", "data_driven_ensemble", "cell_type_unified",
+                     "empty_droplet", "observation_originalid", "alive", "scDblFinder.class"]
+    subdata = cells.drop(columns=[col for col in columns_to_remove if col in cells])
+    pattern = '|'.join(re.escape(s) for s in ["metacell","azimuth","monaco","blueprint","subsets_","high_"])
     
-        anndata = anndata[mask,features].copy()
-        
-        positions_per_cell = []
-        
-        for cid in cell_ids:
-            pos = np.where(anndata.obs.index.str.contains(cid))[0]
-            positions_per_cell.append(pos)
-    
-        ann = anndata[np.concatenate(positions_per_cell).tolist(),:].copy()
-        ann.obs = data.fetchdf()
-        ann.obs.index = ann.obs["cell_id"]
-        
-    elif aggregation == "pseudobulk":
-        cell_ids = data.project('"sample_id"').fetchdf()["sample_id"]+"___"+data.project('"cell_type_unified_ensemble"').fetchdf()["cell_type_unified_ensemble"]
-        ann = anndata[cell_ids.unique(),features].copy()
+    # Find matching columns and drop them
+    cols_to_drop = subdata.columns[subdata.columns.str.contains(pattern, case=False, regex=True)]
+    subdata = subdata.drop(columns=cols_to_drop)
+    subdata = subdata.drop_duplicates(keep='last')
+    subdata.index = subdata["sample_id"]+"___"+subdata["cell_type_unified_ensemble"]
 
-        columns_to_remove = ["cell_id", "cell_type", "file_id_cellNexus_single_cell",
-                         "cell_type_ontology_term_id",
-                         "observation_joinid", "ensemble_joinid",
-                         "nFeature_RNA", "data_driven_ensemble", "cell_type_unified",
-                         "empty_droplet", "observation_originalid", "alive", "scDblFinder.class"]
-        subdata = data.fetchdf().drop(columns=[col for col in columns_to_remove if col in data.fetchdf()])
-        pattern = '|'.join(re.escape(s) for s in ["metacell","azimuth","monaco","blueprint","subsets_","high_"])
-        
-        # Find matching columns and drop them
-        cols_to_drop = subdata.columns[subdata.columns.str.contains(pattern, case=False, regex=True)]
-        subdata = subdata.drop(columns=cols_to_drop)
-        subdata = subdata.drop_duplicates(keep='last')
-        subdata.index = subdata["sample_id"]+"___"+subdata["cell_type_unified_ensemble"]
-
-        ann.obs = subdata.reindex(ann.obs.index)
+    ann.obs = subdata.reindex(ann.obs.index)
     return ann
 
+def filter_metacell(file, data):
+    df = data.filter("file_id_cellNexus_single_cell ="  + "'"+str(file).split("/")[-1]+"'").fetchdf()
+    df["file_id_cellNexus_metacell"] = df["sample_id"].astype(str) + "___" + df["metacell_2"].astype(int).astype(str)
+    df.index = df["file_id_cellNexus_metacell"]
+    filt_ad = ad.read_h5ad(file)[df["file_id_cellNexus_metacell"].unique()]
+    filt_ad.obs = df[["dataset_id", "sample_id", "assay", "assay_ontology_term_id", 
+     "development_stage", "development_stage_ontology_term_id", "disease", "disease_ontology_term_id", 
+     "donor_id", "experiment___", "explorer_url", "feature_count", "is_primary_data", 
+     "organism", "organism_ontology_term_id", "published_at", "raw_data_location", 
+     "revised_at", "sample_heuristic", "schema_version", "self_reported_ethnicity", 
+     "self_reported_ethnicity_ontology_term_id", "sex", "sex_ontology_term_id", "tissue", 
+     "tissue_ontology_term_id", "tissue_type", "title", "tombstone", "url", "age_days", 
+     "tissue_groups", "atlas_id", "sample_chunk", "file_id_cellNexus_single_cell","file_id_cellNexus_metacell"]].drop_duplicates()
+    return filt_ad
+
+def filter_single_cell(file, data):
+    cells = data.filter("file_id_cellNexus_single_cell ="  + "'"+str(file).split("/")[-1]+"'").fetchdf()
+    anndata = ad.read_h5ad(file)
+    anndata.obs.index = anndata.obs.index.astype(str)
+    cell_ids = cells["cell_id"].astype(str)
+    pattern = '|'.join(re.escape(s) for s in cell_ids)
+    mask = anndata.obs.index.str.contains(pattern, regex=True)
+
+    anndata = anndata[mask].copy()
+
+    positions_per_cell = []
+        
+    for cid in cell_ids:
+        pos = np.where(anndata.obs.index.str.contains(cid))[0]
+        positions_per_cell.append(pos)
+
+    ann = anndata[np.concatenate(positions_per_cell).tolist(),:].copy()
+    ann.obs = cells
+    ann.obs.index = ann.obs["cell_id"]
+
+    return ann
+    
 def get_anndata(
     data: duckdb.DuckDBPyRelation,
-    assays: Iterable[Literal["counts", "cpm"]] = ["counts", "cpm"],
-    aggregation: Iterable[Literal["single_cell", "pseudobulk", "metacell"]] = ["single_cell", "pseudobulk", "metacell"],
+    assay: str = "counts",
+    aggregation: str = "single_cell",
     cache_directory: Path = _get_default_cache_dir(),
     repository: str = ASSAY_URL,
-    features: Iterable[str] = slice(None, None, None),
+    features: Iterable = slice(None, None, None)
 ) -> ad.AnnData:
     # error checking
-    assert set(assays).issubset(set(assay_map.keys()))
+    assert assay in (set(assay_map.keys()))
     assert isinstance(cache_directory, Path), "cache_directory must be a Path"
     
-    assays = set(assays)
     cache_directory.mkdir(exist_ok=True, parents=True)
+
+    if aggregation == "metacell_2": data = data.filter(aggregation + " IS NOT NULL")
     
-        # all the files to retrieve for query
-    if str(aggregation[0])=="metacell":
-        aggregation = ["single_cell"] 
-        data = data.filter("metacell_2 IS NOT NULL")
+    if aggregation == "pseudobulk":
+        files_to_read = (
+            data.project("file_id_cellNexus_pseudobulk").distinct().fetchdf()["file_id_cellNexus_pseudobulk"]
+        )
+    else:
+        files_to_read = (
+            data.project("file_id_cellNexus_single_cell").distinct().fetchdf()["file_id_cellNexus_single_cell"]
+        )
     
-    files_to_read = (
-        data.project("file_id_cellNexus_"+str(aggregation[0])).distinct().fetchdf()["file_id_cellNexus_"+str(aggregation[0])]
-    )
-    cells = data.project('"cell_id"').distinct().fetchdf()["cell_id"]
-    atlases = data.project('"atlas_id"').distinct().fetchdf()["atlas_id"]
-        # mapping requested assay to subdirectory name
-    subdirs = [assay_map[a] for a in assays]                                                                                                                         
+    atlas = data.project('"atlas_id"').distinct().fetchdf()["atlas_id"][0]                                                                                                                      
     
     synced = sync_assay_files(
-        url=repository, cache_dir=cache_directory, atlases=atlases, subdirs=subdirs, aggregation=aggregation[0], files=files_to_read
+        url=repository, cache_dir=cache_directory, atlas=atlas, subdir=assay, aggregation=aggregation, files=files_to_read
     )
+
+    if aggregation == "pseudobulk":
+        for _, files in itertools.groupby(synced, key=lambda x: x[0]):
+            ads = [filter_pseudobulk(file[1], data) for file in files]
+    elif aggregation == "metacell_2":
+        for _, files in itertools.groupby(synced, key=lambda x: x[0]):
+            ads = [filter_metacell(file[1], data) for file in files]
+    else:
+        for _, files in itertools.groupby(synced, key=lambda x: x[0]):
+            ads = [filter_single_cell(file[1], data) for file in files]
+
+    adatas = ad.concat(ads,index_unique="_")
     
-    ad_per_assay: dict[str, ad.AnnData] = {}
-        # Concatenate files per assay
-    for assay_name, files in itertools.groupby(synced, key=lambda x: x[0]):
-        ads = [ad.read_h5ad(file[1]) for file in files]
-        ad_per_assay[assay_name] = ad.concat(ads)
-    
-    main_assay_key = (
-        "counts" if "counts" in ad_per_assay else next(iter(ad_per_assay.keys()))
-    )
-    
-    ann = ad_per_assay.pop(main_assay_key)
-        # Attach the additional assays
-    for key, anndata in ad_per_assay.items():
-        ann.layers[key] = anndata.X
-    
-    ann = _filter_obs(data,aggregation[0],ann,cells,features)
-    return ann
+    return adatas[:,features]
